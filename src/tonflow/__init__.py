3294a7fa6b71971abd102619e60df05c32aea2cd
Кахаров Рустам <kaharov95@gmail.com>
2026-06-14 01:34:46 +0600

    feat: add core data models

--- src/tonflow/__init__.py
+++ src/tonflow/__init__.py
@@ -3,14 +3,26 @@
 from tonflow.addresses import is_user_friendly_address, normalize_address
 from tonflow.cache import InMemoryCache
 from tonflow.client import TonClient
-from tonflow.models import JettonTransfer, Message, Transaction
+from tonflow.models import (
+    JettonTransfer,
+    Message,
+    MessageDirection,
+    RawPayload,
+    TonflowModel,
+    Transaction,
+    TransactionStatus,
+)
 
 __all__ = [
     "InMemoryCache",
     "JettonTransfer",
     "Message",
+    "MessageDirection",
+    "RawPayload",
     "TonClient",
+    "TonflowModel",
     "Transaction",
+    "TransactionStatus",
     "is_user_friendly_address",
     "normalize_address",
 ]
